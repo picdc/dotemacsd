1bf1fa2beded17f5222c40e8b77670911da97585
Pierrick Couderc <pierrick.couderc@nomadic-labs.com>
2022-07-25 23:38:11 +0200

    Personal fixes

--- init.el
+++ init.el
@@ -272,13 +272,13 @@ Enabling this leads to a bug where your completion may replace the following wor
   :type 'symbol
   :tag "󰔎 Dark/Light")
 
-(defcustom pokemacs-dark-theme 'doom-solarized-dark
+(defcustom pokemacs-dark-theme 'doom-nord
   "Dark theme to load."
   :group 'pokemacs-appearance
   :type 'symbol
   :tag "󰖔 Dark Theme")
 
-(defcustom pokemacs-light-theme 'doom-solarized-light
+(defcustom pokemacs-light-theme 'doom-nord-light
   "Light theme to load."
   :group 'pokemacs-appearance
   :type 'symbol
@@ -545,7 +545,8 @@ Enabling this leads to a bug where your completion may replace the following wor
   :init
   (add-to-list 'auto-mode-alist '("\\.in\\'" . text-mode))
   (add-to-list 'auto-mode-alist '("\\.out\\'" . text-mode))
-  (add-to-list 'auto-mode-alist '("\\.args\\'" . text-mode)))
+  (add-to-list 'auto-mode-alist '("\\.args\\'" . text-mode))
+(add-to-list 'auto-mode-alist '("\\.wast\\'" . lisp-mode)))
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
@@ -2328,8 +2329,11 @@ debian, and derivatives). On most it's 'fd'.")
          (elixir-mode . lsp-deferred)
          (enh-ruby-mode . lsp-deferred)
          (fsharp-mode . lsp-deferred)
+         (js-mode . lsp-deferred)
+         (js-ts-mode . lsp-deferred)
          (kotlin-mode . lsp-deferred)
          (rustic-mode . lsp-deferred)
+         (typescript-ts-mode . lsp-deferred)
          (tuareg-mode . lsp-deferred)
          (zig-mode    . lsp-deferred)
          (zig-ts-mode    . lsp-deferred))
@@ -5046,6 +5050,10 @@ Return the folder in which rust-analyzer will be started."
     ;; (defun my/rust-mode-outline-regexp-setup ()
     ;;   (setq-local outline-regexp "///[;]\\{1,8\\}[^ \t]"))
     (message "`rustic' loaded")))
+
+(use-package csv-mode
+  :demand t
+  :ensure t)
 
 (when use-sicp
   (use-package sicp))
